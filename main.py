c4dc5840dba90b061dd8fc6950deac52f37ce98e
Benjamin ROECKER <benjamin.roecker@efrei.net>
2020-11-05 16:30:54 +0100

    ajout du choix du graphe

--- main.py
+++ main.py
@@ -2,7 +2,15 @@
 import graphes
 
 if __name__ == '__main__':
-    texte = "graphes/graphe_test.txt"
+    choix = -1
+    while choix < 0 or choix > 50:
+        choix = input("Quelles graphes non-orientés voulez vous choisir ?")
+        try:
+            choix = int(choix)
+        except:
+            choix = 0
+    choix = str(choix)
+    texte = "graphes/graphe_" + choix + ".txt"
     cool = graphes.Graphe(texte)
     cool.matrice_adj()
     cool.floyd_warshall()
