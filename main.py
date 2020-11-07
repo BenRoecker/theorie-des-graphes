91c8b27043a4c2f4d9871f3a67c428202a859b9d
Benjamin ROECKER <benjamin.roecker@efrei.net>
2020-11-07 19:21:28 +0100

    ajout des graphes et amélioration de choix

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ import graphes
 
 if __name__ == '__main__':
     choix = -1
-    while choix < 0 or choix > 50:
-        choix = input("Quelles graphes non-orientés voulez vous choisir ?")
+    choix = input("Quelles graphes orientés voulez vous choisir ? tapez fin pour arreter")
+    while choix != "fin":
         try:
             choix = int(choix)
         except:
             choix = 0
-    choix = str(choix)
-    texte = "graphes/graphe_" + choix + ".txt"
-    cool = graphes.Graphe(texte)
-    cool.matrice_adj()
-    cool.floyd_warshall()
+        while choix < 1 or choix > 13:
+            choix = input("Il n'y a que 13 graphes. ")
+            try:
+                choix = int(choix)
+            except:
+                choix = 0
+        choix = str(choix)
+        texte = "graphes/graphe_" + choix + ".txt"
+        cool = graphes.Graphe(texte)
+        cool.matrice_adj()
+        cool.floyd_warshall()
+        choix = input("Quelles graphes orientés voulez vous choisir ? tapez fin pour arreter")
 
